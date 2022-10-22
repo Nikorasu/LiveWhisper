6793d1fc7e3e434c8dcaa27155baff570a826be2
Nikorasu <nikorasu85@gmail.com>
2022-10-22 17:48:27 -0500

    Can now get current weather & improved day format.

--- assistant.py
+++ assistant.py
@@ -1,5 +1,4 @@
 from scipy.io.wavfile import write
-#from subprocess import call
 import sounddevice as sd
 import numpy as np
 import wikipedia
@@ -9,14 +8,15 @@ import whisper
 import time
 import os
 
-model = 'small'     # Whisper model size (tiny, base, small, medium, large)
-english = True      # Use english-only model?
-translate = False   # Translate non-english to english?
-samplerate = 44100  # Stream device recording frequency
-blocksize = 30      # Block size in milliseconds
-threshold = 0.25    # Minimum volume threshold to activate listening
-vocals = [50, 1000] # Frequency range to detect sounds that could be speech
-endblocks = 30      # Number of blocks to wait before sending to Whisper
+model = 'small'       # Whisper model size (tiny, base, small, medium, large)
+english = True        # Use english-only model?
+translate = False     # Translate non-english to english?
+samplerate = 44100    # Stream device recording frequency
+blocksize = 30        # Block size in milliseconds
+threshold = 0.25      # Minimum volume threshold to activate listening
+vocals = [50, 1000]   # Frequency range to detect sounds that could be speech
+endblocks = 30        # Number of blocks to wait before sending to Whisper
+location = 'Somonauk' # For weather (wttr.in), uses IP location if not specified
 
 class Assistant:
     def __init__(self):
@@ -25,16 +25,18 @@ class Assistant:
         self.espeak = pyttsx3.init()
         self.espeak.setProperty('rate', 180)
         self.askwiki = False
+        self.weatherSave = ['',0]
 
     def analyze(self, input):
-        string = "".join(ch for ch in input if ch not in {",",".","?","!","'"})
-        query = string.lower().split()
-        wikiwords = {"computer","do","a","check","wikipedia","search","for","on","what","whats","who","whos","is","an","does","say","can","you","tell","me","about","of"}
+        string = "".join(ch for ch in input if ch not in {",",".","?","!","'"}) # Remove punctuation
+        query = string.lower().split() # Split into words
+        wikiwords = {"computer","do","a","check","wikipedia","search","for","on","what","whats","who","whos","is","an","does","say","can","you","tell","me","about","of","something"}
+        respond = self.prompted or "computer" in query
         if query == ["computer"]:
             self.speak('Yes?')
             self.prompted = True
-        elif self.askwiki or ((self.prompted or "computer" in query) and "wikipedia" in query):
-            query = [word for word in query if word not in wikiwords]
+        elif self.askwiki or (respond and "wikipedia" in query):
+            query = [word for word in query if word not in wikiwords] # remake query without wikiwords
             if query == [] and not self.askwiki: # if query is empty after removing wikiwords, ask user for search term
                 self.speak("what do you want to search for?")
                 self.askwiki = True
@@ -45,13 +47,31 @@ class Assistant:
                 self.getwiki(" ".join(query))
                 self.askwiki = False
             self.prompted = False
-        elif (self.prompted or "computer" in query) and "time" in query: #any(ele in set for ele in query) #{'what','whats','}
+        elif respond and "time" in query: #any(ele in set for ele in query) #{'what','whats','}
             self.speak("The time is " + time.strftime("%I:%M %p"))
             self.prompted = False
-        elif (self.prompted or "computer" in query) and "date" in query:
-            self.speak("The date is " + time.strftime("%B %d, %Y"))
+        elif respond and "date" in query:
+            day = time.strftime("%e")
+            self.speak("Today's date is " + time.strftime(f"%B {day}{self.orday(int(day))} %Y"))
             self.prompted = False
-        elif (self.prompted or "computer" in query) and "joke" in query or "jokes" in query or "funny" in query:
+        elif respond and "day" in query:
+            day = time.strftime("%e")
+            self.speak("It's " + time.strftime(f"%A the {day}{self.orday(int(day))}"))
+            self.prompted = False
+        elif respond and "weather" in query:
+            curTime = time.time()
+            if curTime - self.weatherSave[1] > 300 or self.weatherSave[1] == 0: # if last weather request was more than 5 minutes ago
+                try:
+                    weather = requests.get(f'http://wttr.in/{location}?format=%C+with+a+temperature+of+%t')
+                    outcome = self.weatherSave[0] = f"Current weather in {location} is {weather.text.replace('+','')}."
+                    self.weatherSave[1] = curTime
+                except requests.exceptions.ConnectionError:
+                    outcome = "I couldn't connect to the weather service."
+            else:
+                outcome = self.weatherSave[0]
+            self.speak(outcome)
+            self.prompted = False
+        elif respond and "joke" in query or "jokes" in query or "funny" in query:
             joke = requests.get('https://icanhazdadjoke.com', headers={"Accept":"text/plain"})
             self.speak(joke.text)
             self.prompted = False
@@ -71,6 +91,9 @@ class Assistant:
         except (wikipedia.exceptions.PageError, wikipedia.exceptions.WikipediaException):
             self.speak("I couldn't find that. Could you be more specific?")
 
+    # One-line function to return ordinal suffix for days of the month. 1st, 2nd, 3rd, 4th, etc.
+    def orday(self,day): return ["th","st","nd","rd"][day%10] if day%10 in [1,2,3] and day not in [11,12,13] else "th"
+
 class StreamHandler:
     def __init__(self, assist):
         self.assist = assist
@@ -88,8 +111,7 @@ class StreamHandler:
             freq = np.argmax(np.abs(np.fft.rfft(indata[:, 0]))) * samplerate / frames
             if indata.max() > threshold and vocals[0] <= freq <= vocals[1] and not self.assist.talking:
                 print('.', end='', flush=True)
-                if self.padding < 1:
-                    self.buffer = self.prevblock.copy()
+                if self.padding < 1: self.buffer = self.prevblock.copy()
                 self.buffer = np.concatenate((self.buffer, indata))
                 self.padding = endblocks
             else:

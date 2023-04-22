e5c85261c2cdb0cc5979017d735b0ca2e29559d5
austenoutoften <austin.bev@hotmail.com>
2023-04-22 15:06:04 +0800

    feat: live transcirption (poor performance)

--- gui.py
+++ gui.py
@@ -4,24 +4,22 @@ import numpy as np
 import sounddevice as sd
 from scipy.io.wavfile import write
 
-Model = 'base'     # Whisper model size (tiny, base, small, medium, large)
+Model = 'tiny'      # Whisper model size (tiny, base, small, medium, large)
 English = True      # Use English-only model?
 Translate = False   # Translate non-English to English?
 SampleRate = 44100  # Stream device recording frequency
 BlockSize = 30      # Block size in milliseconds
 Threshold = 0.1     # Minimum volume threshold to activate listening
 Vocals = [50, 1000] # Frequency range to detect sounds that could be speech
-EndBlocks = 100      # Number of blocks to wait before sending to Whisper
+EndBlocks = 100     # Number of blocks of silence to wait before sending to Whisper
+Internal = 50       # Number of blocks to wait before sending to Whisper (without silence)
 
-class DataValues():
-    clicks = 0
-    frequency = 0
-    status = "Silence"
 
 class StreamHandler:
     def __init__(self):
         self.running = True
         self.padding = 0
+        self.interval = 0
         self.prevblock = self.buffer = np.zeros((0,1))
         self.fileready = False
         print("\033[96mLoading Whisper Model..\033[0m", end='', flush=True)
@@ -50,20 +48,30 @@ class StreamHandler:
             new_value = old_value
         dpg.set_value("progress-bar", new_value)
 
+        # Check if sound in threshold
         if np.sqrt(np.mean(indata**2)) > Threshold and Vocals[0] <= freq <= Vocals[1]:
             print('.', end='', flush=True)
             if self.padding < 1: 
                 self.buffer = self.prevblock.copy()
             self.buffer = np.concatenate((self.buffer, indata))
             self.padding = EndBlocks
+            if (self.interval == 0):
+                self.interval = Internal
         else:
             self.padding -= 1
+            self.interval = 0 if self.interval == 0 else self.interval -1 
+            dpg.set_value("test", self.interval)
+            dpg.set_value("test2", self.buffer.shape[0])
+            if self.interval == 0 and self.buffer.shape[0] > SampleRate:
+                self.fileready = True
+                write('dictate.wav', SampleRate, self.buffer) 
             if self.padding > 1:
                 dpg.set_value("status","Recording...")
                 self.buffer = np.concatenate((self.buffer, indata))
-            elif self.padding < 1 < self.buffer.shape[0] > SampleRate: # if enough silence has passed, write to file.
+            # if enough silence has passed, write to file.
+            elif self.padding < 1 < self.buffer.shape[0] > SampleRate: 
                 self.fileready = True
-                write('dictate.wav', SampleRate, self.buffer) # I'd rather send data to Whisper directly..
+                write('dictate.wav', SampleRate, self.buffer) 
                 self.buffer = np.zeros((0,1))
                 dpg.set_value("status","Silence")
             elif self.padding < 1 < self.buffer.shape[0] < SampleRate: # if recording not long enough, reset buffer.
@@ -80,6 +88,7 @@ class StreamHandler:
             print("\n\033[90mTranscribing..\033[0m")
             result = self.model.transcribe('dictate.wav',fp16=False,language='en' if English else '',task='translate' if Translate else 'transcribe')
             print(f"\033[1A\033[2K\033[0G{result['text']}")
+            dpg.set_value("transcripion", result['text'])
             self.fileready = False
 
     def listen(self):
@@ -88,21 +97,17 @@ class StreamHandler:
             while dpg.is_dearpygui_running():
                 self.process()
 
-
-
 dpg.create_context()
-
-def start_transribing(sender, value, user_data):
-    DataValues.clicks += 1
-    # update text
-    dpg.set_value(user_data, f"Clicks: {DataValues.clicks}")
 
 with dpg.window(label="Live Whisper", tag="Primary Window"):
     textControl = dpg.add_text("Clicks: 0", tag="frequency")
     dpg.add_progress_bar(tag="progress-bar")
     dpg.set_value("progress-bar", 0.3)
     dpg.add_text("Silence", tag="status")
-    #dpg.add_button(label="Click me !", callback=start_transribing, user_data=textControl)
+    dpg.add_text("", tag="transcripion")
+    dpg.add_text("", tag="test")
+    dpg.add_text("", tag="test2")
+    dpg.add_text(SampleRate, tag="test3")
 
 dpg.create_viewport()
 dpg.setup_dearpygui()

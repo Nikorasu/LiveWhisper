90490fe36a0b830d2ee3548f119fdc3cf2ed171c
Antonio J. Delgado <ad@susurrando.com>
2023-05-15 12:23:07 +0300

    Use click for arguments to the class and changed some defaults

--- livewhisper.py
+++ livewhisper.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3
-import whisper, os
+# -*- encoding: utf-8 -*-
+
+import sys
+import os
+import logging
+import click
+import click_config_file
+from logging.handlers import SysLogHandler
+import whisper
 import numpy as np
 import sounddevice as sd
 from scipy.io.wavfile import write
 
 # This is my attempt to make psuedo-live transcription of speech using Whisper.
 # Since my system can't use pyaudio, I'm using sounddevice instead.
-# This terminal implementation can run standalone or imported for assistant.py
+# This terminal implementation can run standalone or imported for assistant.py (maybe not after the change of options to use click)
 # by Nik Stromberg - nikorasu85@gmail.com - MIT 2022 - copilot
-
-Model = 'small'     # Whisper model size (tiny, base, small, medium, large)
-English = True      # Use English-only model?
-Translate = False   # Translate non-English to English?
-SampleRate = 44100  # Stream device recording frequency
-BlockSize = 30      # Block size in milliseconds
-Threshold = 0.1     # Minimum volume threshold to activate listening
-Vocals = [50, 1000] # Frequency range to detect sounds that could be speech
-EndBlocks = 40      # Number of blocks to wait before sending to Whisper
+# and Antonio J. Delgado (2023)
 
 class StreamHandler:
-    def __init__(self, assist=None):
+    def __init__(self, assist=None, **kwargs):
         if assist == None:  # If not being run by my assistant, just run as terminal transcriber.
             class fakeAsst(): running, talking, analyze = True, False, None
             self.asst = fakeAsst()  # anyone know a better way to do this?
         else: self.asst = assist
+        self.kwargs = kwargs
         self.running = True
         self.padding = 0
         self.prevblock = self.buffer = np.zeros((0,1))
         self.fileready = False
         print("\033[96mLoading Whisper Model..\033[0m", end='', flush=True)
-        self.model = whisper.load_model(f'{Model}{".en" if English else ""}')
+        if self.kwargs['language'] == 'en' or self.kwargs['language'] == 'English':
+            model = f"{self.kwargs['model']}.en"
+        else:
+            model = self.kwargs['model']
+        self.model = whisper.load_model(model)
         print("\033[90m Done.\033[0m")
 
     def callback(self, indata, frames, time, status):
@@ -41,21 +46,21 @@ class StreamHandler:
             return
         # A few alternative methods exist for detecting speech.. #indata.max() > Threshold
         #zero_crossing_rate = np.sum(np.abs(np.diff(np.sign(indata)))) / (2 * indata.shape[0]) # threshold 20
-        freq = np.argmax(np.abs(np.fft.rfft(indata[:, 0]))) * SampleRate / frames
-        if np.sqrt(np.mean(indata**2)) > Threshold and Vocals[0] <= freq <= Vocals[1] and not self.asst.talking:
+        freq = np.argmax(np.abs(np.fft.rfft(indata[:, 0]))) * self.kwargs['sample_rate'] / frames
+        if np.sqrt(np.mean(indata**2)) > self.kwargs['threshold'] and self.kwargs['vocals_min'] <= freq <= self.kwargs['vocals_max'] and not self.asst.talking:
             print('.', end='', flush=True)
             if self.padding < 1: self.buffer = self.prevblock.copy()
             self.buffer = np.concatenate((self.buffer, indata))
-            self.padding = EndBlocks
+            self.padding = self.kwargs['end_blocks']
         else:
             self.padding -= 1
             if self.padding > 1:
                 self.buffer = np.concatenate((self.buffer, indata))
-            elif self.padding < 1 < self.buffer.shape[0] > SampleRate: # if enough silence has passed, write to file.
+            elif self.padding < 1 < self.buffer.shape[0] > self.kwargs['sample_rate']: # if enough silence has passed, write to file.
                 self.fileready = True
-                write('dictate.wav', SampleRate, self.buffer) # I'd rather send data to Whisper directly..
+                write('dictate.wav', self.kwargs['sample_rate'], self.buffer) # I'd rather send data to Whisper directly..
                 self.buffer = np.zeros((0,1))
-            elif self.padding < 1 < self.buffer.shape[0] < SampleRate: # if recording not long enough, reset buffer.
+            elif self.padding < 1 < self.buffer.shape[0] < self.kwargs['sample_rate']: # if recording not long enough, reset buffer.
                 self.buffer = np.zeros((0,1))
                 print("\033[2K\033[0G", end='', flush=True)
             else:
@@ -64,19 +69,37 @@ class StreamHandler:
     def process(self):
         if self.fileready:
             print("\n\033[90mTranscribing..\033[0m")
-            result = self.model.transcribe('dictate.wav',fp16=False,language='en' if English else '',task='translate' if Translate else 'transcribe')
+            result = self.model.transcribe('dictate.wav',fp16=False,language=self.kwargs['language'],task='translate' if self.kwargs['translate'] else 'transcribe')
             print(f"\033[1A\033[2K\033[0G{result['text']}")
             if self.asst.analyze != None: self.asst.analyze(result['text'])
             self.fileready = False
 
     def listen(self):
         print("\033[32mListening.. \033[37m(Ctrl+C to Quit)\033[0m")
-        with sd.InputStream(channels=1, callback=self.callback, blocksize=int(SampleRate * BlockSize / 1000), samplerate=SampleRate):
+        with sd.InputStream(channels=1, callback=self.callback, blocksize=int(self.kwargs['sample_rate'] * self.kwargs['block_size'] / 1000), samplerate=self.kwargs['sample_rate']):
             while self.running and self.asst.running: self.process()
 
-def main():
+@click.command()
+@click.option("--debug-level", "-d", default="INFO",
+    type=click.Choice(
+        ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET"],
+        case_sensitive=False,
+    ), help='Set the debug level for the standard output.')
+@click.option('--log-file', '-l', help="File to store all debug messages.")
+#@click.option("--dummy","-n", is_flag=True, help="Don't do anything, just show what would be done.") # Don't forget to add dummy to parameters of main function
+@click.option("--model", "-m", default='small', type=click.Choice(['tiny', 'base', 'small', 'medium', 'large']), help='Whisper model size')
+@click.option("--language", "-L", default=None, type=click.Choice(whisper.tokenizer.LANGUAGES.keys()), help='Language to translate from. Empty to autodetect.')
+@click.option("--translate", "-t", default=True, help='Translate to English')
+@click.option("--sample-rate", "-s", default=44100, help="Stream device recording frequency")
+@click.option("--block-size", "-b", default=30, help="Block size in milliseconds")
+@click.option("--threshold", "-T", default=0.1, help="Minimum volume threshold to activate listening")
+@click.option("--vocals-min", "-v", default=50, help='Minimun value of frequency range to detect sounds that could be speech')
+@click.option("--vocals-max", "-V", default=1000, help="Frequency range to detect sounds that could be speech")
+@click.option("--end-blocks", "-e", default=40, help="Number of blocks to wait before sending to Whisper")
+@click_config_file.configuration_option()
+def main(**kwargs):
     try:
-        handler = StreamHandler()
+        handler = StreamHandler(**kwargs)
         handler.listen()
     except (KeyboardInterrupt, SystemExit): pass
     finally:
